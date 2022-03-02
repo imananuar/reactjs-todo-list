fee7b97223c1af67a95835499b33ef2d724195cc
mianuar <53685685+imananuar@users.noreply.github.com>
2022-03-02 12:34:06 +0800

    Added LOCAL_STORAGE_KEY to conserve state when refreshing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
+
+const LOCAL_STORAGE_KEY = 'react-todo-list-todos';
 
 function App() {
 
   const [todos, setTodos] = useState([]);
   // Todos -> Letak semua todo yang pernah dicatatkan
+
+  useEffect(() => {
+    const storageTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+
+    if (storageTodos) {
+      setTodos(storageTodos);
+    }
+
+  }, [])
 
   const addTodo = (todo) => {
     setTodos([todo, ...todos])
@@ -18,6 +29,12 @@ function App() {
     setTodos(todos.filter(todo => todo.id !== id));
     // REMEMBER: Everytime nak update, guna setTodos
   }
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos]);
+
+
 
   return (
     <div className="App">
